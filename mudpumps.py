c1f2881854b1a1c5f0cbb878ae728d8bccf0cea1
Dwight Ledet <35194827+daledet@users.noreply.github.com>
2019-05-11 08:49:39 +0200

    Add files via upload

---
+++ mudpumps.py
@@ -0,0 +1,13 @@
+import math
+
+class MudPump:
+    def __init__(self, liner_id, stroke_length, efficiency):
+        self.liner_id = liner_id
+        self.stroke_length = stroke_length
+        self.efficiency = efficiency
+
+    def __repr__(self):
+        return '{}, {}, {}'.format(self.liner_id, self.stroke_length, self.efficiency)
+
+mud_pump_one = MudPump(input('Liner ID: '), input('Stroke Length: '), input('Efficiency: '))
+print(mud_pump_one)
