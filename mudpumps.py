d1e1d35ecd94c2e50abea41a66076f6754553eb4
Dwight Ledet <35194827+daledet@users.noreply.github.com>
2019-05-12 08:36:41 +0200

    Add files via upload
    Added Annular Velocity calculations

--- mudpumps.py
+++ mudpumps.py
@@ -7,5 +7,30 @@ class MudPump:
     def __repr__(self):
         return '{}, {}, {}'.format(self.liner_id, self.stroke_length, self.efficiency)
 
-mud_pump_one = MudPump(input('Liner ID: '), input('Stroke Length: '), input('Efficiency: '))
-print(mud_pump_one)
+mud_pump_one_efficiency_bbls = MudPump(7, 12, 0.95)
+mud_pump_two_efficiency_bbls = MudPump(7, 12, 0.95)
+mud_pump_three_efficiency_bbls = MudPump(7, 12, 0.95)
+
+def pumpOutputTriplex(constant, liner_id, two, stroke_length, efficiency): # Import from Hydraulics Math
+    pump_output_triplex = ((constant * (liner_id ** two) * stroke_length) * efficiency)
+    return(pump_output_triplex)
+
+mud_pump_one_output_bbls_per_stroke = pumpOutputTriplex(0.000243,7,2,12,0.95)
+mud_pump_two_output_bbls_per_stroke = pumpOutputTriplex(0.000243,7,2,12,0.95)
+mud_pump_three_output_bbls_per_stroke = pumpOutputTriplex(0.000243,7,2,12,0.95)
+
+mud_pump_one_strokes_per_minute = 75
+mud_pump_two_strokes_per_minute = 75
+mud_pump_three_strokes_per_minute = 75
+
+mud_pump_one_output_bbls_per_minute = mud_pump_one_output_bbls_per_stroke * mud_pump_one_strokes_per_minute
+mud_pump_two_output_bbls_per_minute = mud_pump_two_output_bbls_per_stroke * mud_pump_two_strokes_per_minute
+mud_pump_three_output_bbls_per_minute = mud_pump_three_output_bbls_per_stroke * mud_pump_three_strokes_per_minute
+
+casing_id = 9.313
+drill_pipe_od = 5
+
+annular_capacity = (casing_id ** 2 / 1029.8) - (drill_pipe_od ** 2 / 1029.8)
+
+annular_velocity = (mud_pump_one_output_bbls_per_minute + mud_pump_two_output_bbls_per_minute + mud_pump_three_output_bbls_per_minute) / annular_capacity
+return(annular_velocity)
